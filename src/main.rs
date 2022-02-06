e10491f1155d2458b1d66306d8fac4b62280a5f5
Arthur Soulié <arthur.soulie@tuta.io>
2022-02-06 17:57:42 +0100

    Add painting the window black

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
+use glutin::{
+    event::{
+        Event,
+        WindowEvent
+    },
+    event_loop::{
+        EventLoop,
+        ControlFlow
+    },
+    window::WindowBuilder,
+};
+
 fn main() {
-    let el = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new()
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new()
         .with_title("triangle")
         .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+    let ctx = glutin::ContextBuilder::new().build_windowed(wb, &el).unwrap();
+    let windowed_context = unsafe { ctx.make_current().unwrap() };
 
-    match glutin::ContextBuilder::new().build_windowed(wb, &el) {
-            Ok(ctx) => {
-                let windowed_context = unsafe { ctx.make_current().unwrap() };
-                gl::load_with(|s| windowed_context.get_proc_address(s));
-            },
-            Err(e) => {eprintln!("{:?}", e);},
+    gl::load_with(|s| windowed_context.get_proc_address(s));
+
+    // Setting some OpenGL options and stuff
+    unsafe {
+        gl::ClearColor(0.0, 0.0, 0.0, 1.0);
     }
+
+    // Running the event loOoOop
+    el.run(move |event, _, control_flow| {
+        println!("{:?}", event);
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            }
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT | gl::STENCIL_BUFFER_BIT);
+                }
+                windowed_context.swap_buffers().unwrap();
+            }
+            _ => ()
+        }
+    })
 }
+
